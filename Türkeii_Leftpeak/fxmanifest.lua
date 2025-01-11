07d9e4804d40ec8ecb8ff6fd2fa81caf176f22f1
Türkeii <bhektz@gmail.com>
2025-01-11 04:41:31 +0100

    fivem Leftpeak

---
+++ Türkeii_Leftpeak/fxmanifest.lua
@@ -0,0 +1,10 @@
+fx_version 'cerulean'
+author 'Türkeii - discord.gg/wavescripts'
+description 'Wave Leftpeak'
+game 'gta5'
+lua54 'yes'
+
+client_scripts {
+    'config.lua',
+    'client/main.lua'
+}
